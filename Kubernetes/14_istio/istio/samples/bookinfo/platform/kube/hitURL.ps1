cfb4d699a5bfdcf1d6e4884b66d627d91015e5a9
Charan Baisetty <charan@nextops.in>
2025-12-09 21:06:02 +0530

    kiali jaeger otel code

---
+++ Kubernetes/14_istio/istio/samples/bookinfo/platform/kube/hitURL.ps1
@@ -0,0 +1,17 @@
+﻿$URLs = @(
+  'http://34.57.215.211/productpage' #change the public ip as per your setup
+)
+
+$minSeconds = 1 * 1
+$maxSeconds = 2 * 1
+
+# Loop forever
+while($true){
+  # Send requests to all 3 urls
+  $URLs |ForEach-Object {
+    (Invoke-WebRequest -Uri $_ | Select Content).content
+  }
+
+  # Sleep for a random duration between 10 and 15 minutes
+  Start-Sleep -Seconds (Get-Random -Min $minSeconds -Max $maxSeconds)
+}
