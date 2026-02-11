78db1d187870c7cb15c0cf4330a0da502cb88a56
Charan <charan@nextops.in>
2026-02-11 15:26:26 +0530

    original maven

---
+++ Docker/Original_MsBuild_Maven/Maven Projects/my-app/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String args[]){
+
+		System.out.println("Hello World, Maven");
+
+	}
+
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> 6021cc8a76acc639110316c710149f9e2ecb22e2
